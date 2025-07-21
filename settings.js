eb9889ac5f014223da8ace2d44e2937c8f4fe450
musafauz <musafauz761@gmail.com>
2025-07-21 15:15:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '',
   author: '‎',
   botName: ",
-  botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'musa', // Your name
+  ownerNumber: '256784079946', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
